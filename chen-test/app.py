9bc76320615c6ac628c40abab189f25a59d72c2d
chen <flanky@examplye.com>
2020-03-09 16:26:25 +0800

    echart for graph

--- chen-test/app.py
+++ chen-test/app.py
@@ -1,8 +1,10 @@
 from flask import  Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask import  render_template
+from flask import  request,jsonify
+from flask import url_for
 import config
-
+import testsql
 
 app = Flask(__name__)
 app.config.from_object(config)
@@ -21,9 +23,19 @@ class SysMonitInfo(db.Model):
     meminfo = db.Column(db.String(10))
 
 
-@app.route('/index')
+
+@app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/viewdata',methods=['post'])
+def viewdata():
+    if request.method == 'post':
+        res = testsql.selectByParameters('select cpu1mload,cpu5mload,meminfo from sysmonitinfo;')
+        return jsonify(sys_cpu1mload = [x[0] for x in res],
+                       sys_cpu5mload = [x[1] for x in res],
+                       mem_info =[x[2] for x in res])
+
 
 if __name__ == '__main__':
     app.run(host='localhost',port='80',debug=True)
